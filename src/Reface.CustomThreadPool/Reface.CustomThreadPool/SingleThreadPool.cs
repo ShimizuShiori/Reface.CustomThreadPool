d2151c76a8196fd6a1e3fedc4a2416bbd5e85d6d
Felix <116971263@qq.com>
2021-04-19 16:34:04 +0800

    single

---
+++ src/Reface.CustomThreadPool/Reface.CustomThreadPool/SingleThreadPool.cs
@@ -0,0 +1,13 @@
+﻿namespace Reface.CustomThreadPool
+{
+    /// <summary>
+    /// 单个线程的线程池
+    /// </summary>
+    public class SingleThreadPool : FixedSizeThreadPool
+    {
+        public SingleThreadPool() : base(new FixedSizeThreadPoolOption() { Size = 1 })
+        {
+
+        }
+    }
+}
